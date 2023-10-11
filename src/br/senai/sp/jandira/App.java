d647c983b92ba7286472af15af12bbf9d06b44ea
Vitoria <vazevedo73@gmail.com>
2023-10-11 17:29:57 -0300

    iniciado

---
+++ src/br/senai/sp/jandira/App.java
@@ -0,0 +1,11 @@
+package br.senai.sp.jandira;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/br/senai/sp/jandira/model/Gerador.java
@@ -0,0 +1,20 @@
+package br.senai.sp.jandira.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Gerador {
+    Scanner scanner = new Scanner(System.in);
+    String nome;
+    public void geradorNomes(){
+
+
+        System.out.println("Bem-vindo ao gerador de nomes de cachorros!!");
+        System.out.println("Insira os nomes que deseja sortear: ");
+
+    }
+
+
+
+}
