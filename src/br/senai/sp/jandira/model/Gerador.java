a588394cc6e153a0747b50785975b7ffe1cc0ff5
Vitoria <vazevedo73@gmail.com>
2023-10-18 17:28:04 -0300

    .

--- src/br/senai/sp/jandira/model/Gerador.java
+++ src/br/senai/sp/jandira/model/Gerador.java
@@ -6,17 +6,16 @@ import java.util.Scanner;
 
 public class Gerador {
     Scanner scanner = new Scanner(System.in);
-    String nome;
+    String nomes;
     List<Gerador> listGerador = new ArrayList<>();
     public void inserirNomes(){
         System.out.println("Bem-vindo ao gerador de nomes de cachorros!!");
         System.out.println("Insira os nomes que deseja sortear: ");
-        scanner.nextLine();
-
+        nomes = scanner.nextLine();
     }
 
-    public void adicionarNomes(){
-
+    public void adicionarNomes(Gerador gerador){
+        listGerador.add(gerador);
     }
 
 
